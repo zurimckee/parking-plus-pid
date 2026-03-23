abb82c8d37a9518517f0f5ff517147495c3685d6
Zuri McKee <zurimckee95@gmail.com>
2026-03-23 12:43:03 -0400

    solidified db using yt video to put together python gui

--- sqlstuff/parkingplusdb.sql
+++ sqlstuff/parkingplusdb.sql
@@ -9,8 +9,12 @@ CREATE TABLE Spots(
     Geometry_ID MEDIUMINT UNSIGNED NOT NULL, 
     Level SMALLINT UNSIGNED,
     PRIMARY KEY (Spot_ID),
-    FOREIGN KEY (Lot_ID) REFERENCES Lots(Lot_ID) ON DELETE CASCADE 
+    FOREIGN KEY (Lot_ID) REFERENCES Lots(Lot_ID) 
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE
 );
+ALTER TABLE Spots ADD last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
 CREATE TABLE Lots(
 	Lot_ID CHAR(4) UNIQUE,
     Lot_Name VARCHAR(50) NOT NULL,
@@ -24,17 +28,65 @@ CREATE TABLE Lots(
     PRIMARY KEY(Lot_ID)
     );
 
-ALTER TABLE Spots
-DROP FOREIGN KEY spots_ibfk_1;
+-- dynamically retrieves data from defined tables whenever its queried by frontend
+CREATE VIEW Lot_Status_Summary AS 
+SELECT 
+	Lot.Lot_ID,
+    Lot.Lot_Name,
+    Lot.Total_Spaces, 
+    COUNT(Spot.Spot_ID) AS Occupied_Count, 
+    (Lot.Total_Spaces - COUNT(Spot.Spot_ID)) AS Num_Spaces
+FROM Lots Lot
+LEFT JOIN Spots Spot ON Lot.Lot_ID = Spot.Lot_ID AND Spot.Status = TRUE
+GROUP BY Lot.Lot_ID;
 
-ALTER TABLE Spots 
-ADD CONSTRAINT fk_lot_id
-FOREIGN KEY (Lot_ID) REFERENCES Lots(Lot_ID)
-ON DELETE CASCADE
-ON UPDATE CASCADE;
+-- GROUPS DATA BY HOUR/DAY/WEEK FOR USAGE TRENDS
+CREATE TABLE Occupancy_Logs(
+	Log_ID INT AUTO_INCREMENT,
+    Spot_ID MEDIUMINT UNSIGNED UNIQUE NOT NULL,
+    New_Status BOOLEAN NOT NULL,
+    Time_Stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (Log_ID),
+	FOREIGN KEY(Spot_ID) REFERENCES Spots(Spot_ID)
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE
+);
+
+-- trigger : whenever a row in Spots is updated check the Status col for changes, if so alter occupancy_logs table
+DELIMITER //
+CREATE TRIGGER Check_Spot_Stat_Update
+AFTER UPDATE ON Spots
+FOR EACH ROW
+BEGIN
+	IF OLD.Status <> NEW.Status THEN
+		INSERT INTO Occupancy_Logs(Spot_ID, New_Status)
+        VALUES(NEW.Spot_ID, NEW.Status);
+	END IF;
+END //
+DELIMITER ;
+
+
+
+
+-- for error logging and system alerts for sensors
+CREATE TABLE Sensor_Health(
+	Sensor_ID MEDIUMINT UNSIGNED,
+    Last_Ping TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    Is_Online BOOLEAN DEFAULT TRUE,
+    FOREIGN KEY(Sensor_ID) REFERENCES Spots(Spot_ID) 
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE
+);
+
+
+
+    
+    
+
 
 SHOW CREATE TABLE Spots;
 SHOW CREATE TABLE Lots;
+SHOW CREATE TABLE Occupancy_Logs;
+SHOW CREATE TABLE Sensor_Health;
 
-DROP TABLE Spots;
-DROP TABLE Lots;
+
